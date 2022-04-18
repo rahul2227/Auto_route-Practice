281b1c1e9aeae976f262df403f8166790ceaa7c9
RahulSharma-01 <rahul2227.l115@gmail.com>
2022-04-18 15:43:48 +0530

    added and updated dependencies main - initialised firebase application post page - handle navigation to the detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_bottom_navigation_with_nested_routing_tutorial/deep_links/deep_link_page.dart';
 import 'package:flutter_bottom_navigation_with_nested_routing_tutorial/posts/posts_page.dart';
 
 import '../routes/routes.gr.dart';
 import '../posts/post_guard.dart';
 
-void main() {
+void main() async {
+  await init();
   runApp(AppWidget());
+}
+
+Future init() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
 
 class AppWidget extends StatefulWidget {
@@ -20,30 +28,47 @@ class AppWidget extends StatefulWidget {
 
 class _AppWidgetState extends State<AppWidget> {
   final _appRouter = NavigationRouter(postGuard: PostGuard());
-
   var isLoggedIn = false;
 
-  @override
-  void initState() {
-    initDynamicLinks();
-    super.initState();
-  }
+  // FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
+  // @override
+  // void initState() {
+  //   initDynamicLinks();
+  //   super.initState();
+  // }
+
+  // Future<void> initDynamicLinks() async {
+  //   dynamicLinks.onLink.listen((dynamicLinkData) {
+  //     // print(
+  //     //     "This is the Deep Link path parameter ${dynamicLinkData.link.path}");
+  //     // Navigator.pushNamed(context, dynamicLinkData.link.path);
+  //     print(
+  //         "This is the Deep Link path parameter ${dynamicLinkData.link.queryParameters.values.first}");
+  //     Navigator.pushNamed(
+  //         context, dynamicLinkData.link.queryParameters.values.first);
+  //     // context.router
+  //     //     .pushNamed(dynamicLinkData.link.queryParameters.values.first);
+  //   }).onError((error) {
+  //     print('onLink error');
+  //     print(error.message);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Bottom Nav Bar with Nested Routing',
-      home: PostsPage(),
-      routes: {'/settings': ((context) => const DeepLinkPage())},
-    );
-
-    // return MaterialApp.router(
-    //   routeInformationParser: _appRouter.defaultRouteParser(),
-    //   routerDelegate: _appRouter.delegate(),
+    // return MaterialApp(
     //   debugShowCheckedModeBanner: false,
     //   title: 'Bottom Nav Bar with Nested Routing',
+    //   home: PostsPage(),
+    //   routes: {'/settings': ((context) => const DeepLinkPage())},
     // );
+
+    return MaterialApp.router(
+      routeInformationParser: _appRouter.defaultRouteParser(),
+      routerDelegate: _appRouter.delegate(),
+      debugShowCheckedModeBanner: false,
+      title: 'Bottom Nav Bar with Nested Routing',
+    );
 
     // return MaterialApp.router(
     //   routeInformationParser: _appRouter.defaultRouteParser(
@@ -63,40 +88,6 @@ class _AppWidgetState extends State<AppWidget> {
     // );
   }
 
-  // intial function
-  void initDynamicLinks() async {
-    // if (!_isfirst) return;
-    // _isfirst = false;
-    FirebaseDynamicLinks.instance.onLink(
-        onSuccess: (PendingDynamicLinkData? dynamicLinkData) async {
-      final Uri? deeplink = dynamicLinkData?.link;
-      if (deeplink != null) {
-        print("deeplink data " + deeplink.queryParameters.values.first);
-
-        print('/${deeplink.pathSegments.elementAt(1)}');
-
-        AutoRouter.of(context)
-            .navigateNamed('/${deeplink.pathSegments.elementAt(1)}');
-        // Navigator.of(context).pushNamed(deeplink.queryParameters.values.first);
-
-        //print('/${deeplink.pathSegments.elementAt(1)}');
-
-        //Navigator.pushNamed(context, '/${deeplink.pathSegments.elementAt(1)}');
-        // Navigator.push
-        // print("Should have done the work");
-        // setState(() {
-        //   _deepIndex = int.parse(deeplink.queryParameters.values.first);
-        //   print('This is the current index that should change' +
-        //       _deepIndex.toString());
-        // });
-      }
-      //  _changeisLoadingState();
-    }, onError: (OnLinkErrorException e) async {
-      print(e.message);
-      // _changeisLoadingState();
-    });
-  }
-
   onResult(BuildContext context, bool isSuccess) {
     String mesg;
     if (isSuccess) {
@@ -110,3 +101,39 @@ class _AppWidgetState extends State<AppWidget> {
     print(mesg);
   }
 }
+
+/*
+// intial function
+  void initDynamicLinks() async {
+    if (!_isfirst) return;
+    _isfirst = false;
+    FirebaseDynamicLinks.instance.onLink(
+        onSuccess: (PendingDynamicLinkData? dynamicLinkData) async {
+      final Uri? deeplink = dynamicLinkData?.link;
+      if (deeplink != null) {
+        print("deeplink data " + deeplink.queryParameters.values.first);
+
+        print('/${deeplink.pathSegments.elementAt(1)}');
+
+        AutoRouter.of(context)
+            .navigateNamed('/${deeplink.pathSegments.elementAt(1)}');
+        Navigator.of(context).pushNamed(deeplink.queryParameters.values.first);
+
+        print('/${deeplink.pathSegments.elementAt(1)}');
+
+        Navigator.pushNamed(context, '/${deeplink.pathSegments.elementAt(1)}');
+        Navigator.push
+        print("Should have done the work");
+        setState(() {
+          _deepIndex = int.parse(deeplink.queryParameters.values.first);
+          print('This is the current index that should change' +
+              _deepIndex.toString());
+        });
+      }
+      //  _changeisLoadingState();
+    }, onError: (OnLinkErrorException e) async {
+      print(e.message);
+      // _changeisLoadingState();
+    });
+  }
+  */
